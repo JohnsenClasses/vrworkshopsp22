1ae4013321642e1450a7e28f9872799e00b07d8c
Kyle Johnsen <kjjohnsen@gmail.com>
2022-02-04 10:13:11 -0500

    have a button pressing

---
+++ wrworkshopunityproject/Assets/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	
+    //awake is called before all starts in other scripts
+	private void Awake()
+	{
+        Debug.Log("Awake");
+	}
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Update");
+    }
+
+    // Late Update is called after all other updates in all scripts
+	private void LateUpdate()
+	{
+        Debug.Log("LateUpdate");
+	}
+}
